443844f71506a7bf0b5bb49152dc7584c7129a4d
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-03-17 16:40:45 +0100

    [First commit]  Html layout + js workflow

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+/*
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito23
+*/
+
+/* 
+Chiedo i dati all'utente tramite un prompt, e salvo tutte le sue risposte in delle variabili.
+Per creare la "password personalizzata" e mostrarla all'utente utilizzerò una variabile definita "outputPassword" che verrà stampata all'interno di un tag html.
+*/
+
+
+
